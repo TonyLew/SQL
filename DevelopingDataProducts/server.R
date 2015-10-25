727d33bf072d34355efb0a896b4eb6c5d8a50f06
Tony Lew <Tone_Lew@yahoo.com>
2015-10-25 14:50:57 -0700

    Developing Data Products Project along with RPubs presentation file.

--- DevelopingDataProducts/server.R
+++ DevelopingDataProducts/server.R
@@ -1,49 +1,86 @@
 
-  library(shiny)
+#install.packages('googleVis')
+library(googleVis)
+library(shiny)
+library(sqldf)
 
-  shinyServer(
-  function(input,output) {
-    
-    purchase <- reactive({as.numeric(input$purchase)})
-    loanamt <- reactive({as.numeric(input$loanamt)})
-    loanyears <- reactive({as.numeric(input$loanyears)})
-    iratey <- reactive({as.numeric(input$iratey)/100})
-    propertytax <- reactive({as.numeric(input$ptax)/100})   
-    insurance <- reactive({as.numeric(input$insurance)})
-    rent <- reactive({as.numeric(input$rent)})   
-    iratem <- reactive({(iratey()/100)/12})
-    payments <- reactive({loanyears()*12})
-    ownmonthlyamount <- payments
-    propertytaxm <- reactive({(propertytax()*purchase()) / 12})
-    insurancem <- reactive({insurance()/12})
-    a <- reactive({iratem()*loanamt()*((1+iratem())^payments())/(((1+iratem())^payments()) - 1)})
-    ownmonthlyamount <- reactive({a() + propertytaxm() + insurancem()})
 
-    output$purchase <- renderPrint({input$purchase})
-    output$loanamt <- renderPrint({input$loanamt})
-    output$loanyears <- renderPrint({input$loanyears})
-    output$iratey <- renderPrint({input$iratey})
-    output$ptax <- renderPrint({input$ptax})
-    output$insurance <- renderPrint({input$insurance})
-    output$rent <- renderPrint({input$rent})
-    output$ownmonthlyamount <- renderPrint({toString(ownmonthlyamount())})
+dPopulationFemale <- read.csv("PopulationFemale.csv")
+dPopulationForeign <- read.csv("PopulationForeign.csv")
 
-    rentpayoffmonth <- reactive({as.integer((loanamt()/rent())+1)})
-    output$rentpayoffmonth <- renderPrint({rentpayoffmonth()})
+shinyServer(function(input,output) {
 
-    # for (i in 1:payments)
-    # {
-    #  renti <- renti + rent
-    #  rentmonthlyamounts <- rbind(rentmonthlyamounts,renti)
-    #  i <- i + 1
-    # }
-        
-    # output$ROPlot <- renderPlot({
-    # plot(rentmonthlyamounts,xlab='Months of Rent',ylab='Home Loan Amount',main='Rent VS Own')
-    # abline(h=loanamt, col = "red")    
-    # })
-    
-  }
+  output$instructions <- renderText({
+    paste0(
+      "Instructions: \n",
+      "       The side panel is where report choices are made. \n",
+      "       Choose a report, step through years, and  \n",
+      "       select display options for the table. \n",
+      "       Please note that these percentages are for each country's population. \n",
+      "       Also, please be patient as it may take some time to color the world map. \n"
+    )
+  })
+  
+  output$credits <- renderText({
+    paste0(
+      "Credits: \n",
+      "       All data used in these reports were extracted from \n",
+      "       http://www.worldbank.org/."
+    )
+  })
+  
+  output$chosen <- renderText({
+    paste0(
+      "Your Choice: \n",
+      "       Here are the graphs for your chosen dataset and year: \n",
+      "         ",input$dataset, " ", input$year
+    )
+  })
+
+  
+  
+  populationdata <- reactive({  
+    dPFor <- paste0("select CountryCode, CountryName, printf('%.2f', Percentage) as Percentage ",
+                    "from dPopulationForeign x ",
+                    "where Year = '",as.character(input$year),"'"
+    )
+    dPFem <- paste0("select CountryCode, CountryName, printf('%.2f', Percentage) as Percentage ",
+                    "from dPopulationFemale x ",
+                    "where Year = '",as.character(input$year),"'"
+    )
+    choice <- switch(input$dataset, "Population Of Foreigners"=dPFor, "Population Of Females"=dPFem) 
+    data.frame(sqldf(choice))
+  })
+  
+  populationdatamap <- reactive({  
+    dPFor <- paste0("select CountryCode, printf('%.2f', Percentage) as Percentage ",
+                    "from dPopulationForeign x ",
+                    "where Year = '",as.character(input$year),"'"
+    )
+    dPFem <- paste0("select CountryCode, printf('%.2f', Percentage) as Percentage ",
+                    "from dPopulationFemale x ",
+                    "where Year = '",as.character(input$year),"'"
+    )
+    choice <- switch(input$dataset, "Population Of Foreigners"=dPFor, "Population Of Females"=dPFem) 
+    data.frame(sqldf(choice))
+  })
+  
+  
+  output$geochart <- renderGvis({  
+    gvisGeoChart(data=populationdatamap(),
+                 locationvar="CountryCode", colorvar="Percentage"
+                 )
+})  
+  
+  myOptions <- reactive({ 
+    list( page=ifelse(input$pageable==TRUE,'enable','disable'), pageSize=input$pagesize, width=550 ) 
+  }) 
+  
+  output$geotable <- renderGvis({ 
+    gvisTable( data=populationdata(),options=myOptions() ) 
+  })
+  
+  
+}
 )
-
 
